ee9819360ebee1589a2427929affe7c18e7b1681
Yoonll <2207561293@qq.com>
2023-02-18 11:11:55 +0800

    first commit

---
+++ src/main/java/com/yoon/Main.java
@@ -0,0 +1,11 @@
+package com.yoon;
+
+/**
+ * @author yoonll
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
